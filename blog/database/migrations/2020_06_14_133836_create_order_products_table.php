6a7e318dbc98ec710a10bf2a1322fe29dc1c3f8c
valbdes <valbdesik@gmail.com>
2020-06-14 17:07:08 +0300

    completed

--- blog/database/migrations/2020_06_14_133836_create_order_products_table.php
+++ blog/database/migrations/2020_06_14_133836_create_order_products_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 class CreateOrderProductsTable extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrations
      *
      * @return void
      */
